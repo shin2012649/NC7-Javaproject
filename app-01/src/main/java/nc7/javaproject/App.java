9d1104bf7fcf134a72184d2f89b8717a5ced6a2a
shin2012649 <shin2012649@gmail.com>
2023-06-07 06:29:17 +0900

    #2 변수 사용 및 데이터 저장

--- app-01/src/main/java/nc7/javaproject/App.java
+++ app-01/src/main/java/nc7/javaproject/App.java
@@ -4,11 +4,22 @@
 package nc7.javaproject;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+      System.out.println("영화 A 관객 선호도 조사");
+      System.out.println("----------------------------------");
+  
+      System.out.print("참여자 번호: ");
+      System.out.println(100);
+  
+      System.out.printf("참여자 이름: %s", "홍길동");
+      System.out.println();
+  
+      System.out.println("참여자 나이: " + 20);
+  
+      System.out.printf("현재 영화 A를 관람 중인지: %b\n", true);
+  
+      System.out.printf("참여자 성별(M: 남자, W: 여자): %c\n", 'M');
+  
+      System.out.printf("영화 A에 대한 평가(1-5): %d\n", 4);
     }
-}
+  }
