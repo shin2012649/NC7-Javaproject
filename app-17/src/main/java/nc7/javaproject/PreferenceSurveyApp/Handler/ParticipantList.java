97defee88254eaf242c2501ad3a7fccafa63c87f
shin2012649 <shin2012649@gmail.com>
2023-06-15 21:47:53 +0900

    인스턴스 목록 제어 기능을 별도의 클래스로 캡슐화

---
+++ app-17/src/main/java/nc7/javaproject/PreferenceSurveyApp/Handler/ParticipantList.java
@@ -0,0 +1,83 @@
+package nc7.javaproject.PreferenceSurveyApp.Handler;
+import nc7.javaproject.vo.Participant;
+
+public class ParticipantList {
+  private static final int DEFAULT_SIZE = 3;
+  private Participant[] participants = new Participant[DEFAULT_SIZE];
+  private int length;
+  private int[] ratingCounts = new int[5];
+  private int totalScore = 0;
+  private int participantCount = 0;  
+
+public boolean add(Participant p){
+  if (this.length == participants.length) {
+    increase();
+  }
+  this.participants[this.length++] = p;
+  return true;
+}
+
+private void increase(){
+  Participant[] arr = new Participant[participants.length +(participants.length >> 1)];
+  for (int i = 0; i < participants.length; i++){
+    arr[i] = participants[i];
+  }
+  participants = arr;
+  System.out.println("배열확장:" + participants.length);
+}
+
+public int[] getRatingCounts() {
+  return ratingCounts;
+}
+
+public int getTotalScore() {
+  return totalScore;
+}
+
+public int getParticipantCount() {
+  return participantCount;
+}
+
+
+
+public Participant[] list() {
+  Participant[] arr = new Participant[this.length];
+  for (int i = 0; i < this.length; i++) {
+    arr[i] = this.participants[i];
+  } 
+  return arr;
+}
+
+public Participant get(int no){
+  for (int i = 0; i < this.length; i++) {
+    Participant p = this.participants[i];
+    if(p.getNo() == no){
+      return p;
+    }
+  }
+  return null;
+}
+
+public boolean delete(int no){
+  int deletedIndex = indexOf(no);
+    if (deletedIndex == -1) {
+      return false;
+    }
+
+    for (int i = deletedIndex; i < this.length - 1; i++) {
+      this.participants[i] = this.participants[i + 1];
+    }
+    this.participants[--this.length] = null;
+    return true;
+  }
+
+  private int indexOf(int participantNo) {
+    for (int i = 0; i < this.length; i++) {
+      Participant p = this.participants[i];
+      if (p.getNo() == participantNo) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
