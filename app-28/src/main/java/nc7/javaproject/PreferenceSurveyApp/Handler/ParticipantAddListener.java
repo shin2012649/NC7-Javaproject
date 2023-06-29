775acf0b4c6244d7adbc60bb621eb57145752d3c
shin2012649 <shin2012649@gmail.com>
2023-06-29 18:58:33 +0900

    입출력 성능을 높이기 위해 버퍼 기능 추가하기

---
+++ app-28/src/main/java/nc7/javaproject/PreferenceSurveyApp/Handler/ParticipantAddListener.java
@@ -0,0 +1,26 @@
+package nc7.javaproject.PreferenceSurveyApp.Handler;
+import java.util.List;
+import nc7.javaproject.util.BreadcrumbPrompt;
+import nc7.javaproject.vo.Participant;
+
+
+public class ParticipantAddListener extends AbstractParticipantListener {
+  
+  public ParticipantAddListener(List<Participant> list){
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    Participant p = new Participant();
+    p.setName(prompt.inputString("참여자 이름? "));
+    p.setAge(Integer.parseInt(prompt.inputString("참여자 나이? ")));
+    p.setMovieAttendance(prompt.inputString("영화 A 재관람 의사(Y/N)? "));
+    p.setGender(inputGender((char)0,prompt));
+    p.setMovieRating(Integer.parseInt(prompt.inputString("영화 A에 대한 평점(1-5)? ")));
+    p.setAdditionalInfo(prompt.inputString("추가 정보? "));
+    
+    this.list.add(p);
+    }
+  }
+    
---
+++ app-28/src/main/java/nc7/javaproject/PreferenceSurveyApp/Handler/ParticipantDetailListener.java
@@ -0,0 +1,34 @@
+package nc7.javaproject.PreferenceSurveyApp.Handler;
+import java.util.List;
+import nc7.javaproject.util.BreadcrumbPrompt;
+import nc7.javaproject.vo.Participant;
+
+
+public class ParticipantDetailListener extends AbstractParticipantListener{
+
+
+  
+  public ParticipantDetailListener(List<Participant> list){
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    int participantNo = prompt.inputInt("조회할 참여자 번호? ");
+    Participant p = this.findBy(participantNo);
+      if (p == null) {
+        System.out.println("해당 번호의 참여자를 찾을 수 없습니다.");
+        return;
+      }
+        System.out.printf("참여자 이름: %s\n", p.getName());
+        System.out.printf("참여자 나이: %d\n", p.getAge());
+        System.out.printf("영화  재관람 의사: %s\n", p.getMovieAttendance());
+        System.out.printf("성별: %s\n", toGenderString(p.getGender()));
+        System.out.printf("영화 A에 대한 평가(1-5): %d\n", p.getMovieRating());
+        System.out.printf("추가 정보: %s\n", p.getAdditionalInfo());
+    }
+  }
+    
+
+
+  
