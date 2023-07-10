7146c569a8bff5d8b63697cc5bf6ebadc18afc42
shin2012649 <shin2012649@gmail.com>
2023-07-10 19:24:52 +0900

    Reflection API를 활용하여 DAO 메서드 호출을 자동화하기

---
+++ app-39/app-client/src/main/java/nc7/javaproject/util/Menu.java
@@ -0,0 +1,36 @@
+package nc7.javaproject.util;
+
+import java.util.ArrayList;
+
+public class Menu {
+
+  private String title;
+  private ArrayList<ActionListener> listeners = new ArrayList<>();
+
+  public Menu(String title) {
+    this.title = title;
+  }
+
+  public Menu(String title, ActionListener listener) {
+    this(title);
+    this.addActionListener(listener);
+  }
+
+  public void addActionListener(ActionListener listener) {
+    listeners.add(listener);
+  }
+
+  public void removeActionListener(ActionListener listener) {
+    listeners.remove(listener);
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void execute(BreadcrumbPrompt prompt) {
+    for (ActionListener listener : listeners) {
+      listener.service(prompt);
+    }
+  }
+}
