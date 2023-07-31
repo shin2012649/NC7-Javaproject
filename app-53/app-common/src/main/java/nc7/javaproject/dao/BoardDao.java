f72b2b91b767a581844cd541b1886ae9535b07cf
shin2012649 <shin2012649@gmail.com>
2023-07-31 19:35:42 +0900

    리스너 실행에 Facade 패턴 적용하기

--- app-53/app-common/src/main/java/nc7/javaproject/dao/BoardDao.java
+++ app-53/app-common/src/main/java/nc7/javaproject/dao/BoardDao.java
@@ -7,7 +7,7 @@ import nc7.javaproject.vo.Board;
 public interface BoardDao {
   void insert(Board board);
   List<Board> findAll(int category);
-  Board findBy(int category, int no);
+  Board findBy(int category, int  no);
   int update(Board board);
   int updateCount(Board board);
   int delete(Board board);
