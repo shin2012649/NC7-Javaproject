160a3160083567225b32f967c3aaa5c82aed9e5f
shin2012649 <shin2012649@gmail.com>
2023-08-02 17:42:19 +0900

    IoC 컨테이너 적용하기

---
+++ app-54/app-server/src/main/java/nc7/javaproject/Handler/HeadListener.java
@@ -0,0 +1,12 @@
+package nc7.javaproject.Handler;
+
+import nc7.util.ActionListener;
+import nc7.util.BreadcrumbPrompt;
+
+public class HeadListener implements ActionListener{
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    prompt.println("=============================[오호]==");
+  }
+}
