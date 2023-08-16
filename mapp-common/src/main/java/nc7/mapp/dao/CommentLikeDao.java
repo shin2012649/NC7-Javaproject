666b57dbbcc0a33cbe7bd673d242873cd464e821
shin2012649 <shin2012649@gmail.com>
2023-08-16 21:32:11 +0900

    update

--- mapp-common/src/main/java/nc7/mapp/dao/CommentLikeDao.java
+++
@@ -1,9 +0,0 @@
-package nc7.mapp.dao;
-
-import nc7.mapp.vo.CommentLike;
-
-public interface CommentLikeDao {
-    void insert(CommentLike commentLike);
-    boolean exists(int commentsNo, int usersNo);
-    int delete(int commentsNo, int usersNo);
-}
