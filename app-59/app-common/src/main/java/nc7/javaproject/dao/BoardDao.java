2c93abda8485d7b1643e0d9b52dfe960d44a33f6
shin2012649 <shin2012649@gmail.com>
2023-08-19 22:01:30 +0900

    서블릿 컨테이너 삽입하기

---
+++ app-59/app-common/src/main/java/nc7/javaproject/dao/BoardDao.java
@@ -0,0 +1,19 @@
+package nc7.javaproject.dao;
+
+import java.util.List;
+import nc7.javaproject.vo.AttachedFile;
+import nc7.javaproject.vo.Board;
+
+
+public interface BoardDao {
+  void insert(Board board);
+  List<Board> findAll(int category);
+  Board findBy(int category, int no);
+  int update(Board board);
+  int updateCount(Board board);
+  int delete(Board board);
+
+  int insertFiles(Board board);
+  AttachedFile findFileBy(int no);
+  int deleteFile(int fileNo);
+}
